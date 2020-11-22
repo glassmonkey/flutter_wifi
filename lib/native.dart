0947cf1111b9bc2f9f44f81fe6c25b6813b66f3a
永野峻輔 <peace.infi0407@gmail.com>
2020-11-22 21:32:22 +0900

    update

--- lib/native.dart
+++ lib/native.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_wifi_sample/native/buttery_api.dart';
+import 'package:flutter_wifi_sample/native/api.dart';
 
 class WifiView extends StatefulWidget {
   @override
